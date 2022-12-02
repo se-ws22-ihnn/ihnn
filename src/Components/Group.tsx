27ceb4a3f7bf12e586783a7d19087f19671d5bdc
Soeren Helms <soeren.helms@repronik.it>
2022-12-02 16:16:25 +0100

    working on users and group #18

--- src/Components/Group.tsx
+++ src/Components/Group.tsx
@@ -1,53 +1,53 @@
-import * as React from "react";
-import { useState } from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import * as React from 'react';
+import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import {
-	Avatar,
-	Button,
-	FormControl,
-	IconButton,
-	InputLabel,
-	MenuItem,
-	TextField,
-} from "@mui/material";
+    Avatar,
+    Button,
+    FormControl,
+    IconButton,
+    InputLabel,
+    MenuItem,
+    TextField,
+} from '@mui/material';
 // Table Imports
 import {
-	Table,
-	TableHead,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableRow,
-} from "@mui/material";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { Player } from "../types/playerType";
+    Table,
+    TableHead,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+} from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Player } from '../types/playerType';
 // icon imports
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import DeleteIcon from "@mui/icons-material/Delete";
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 // own palette of colors available for the avatar
 const avatarcolors = {
-	pink: "#e91e63",
-	purple: "#673ab7",
-	blue: "#2196f3",
-	teal: "#009688",
-	green: "#4caf50",
-	lime: "#cddc39",
-	brown: "#795548",
+    pink: '#e91e63',
+    purple: '#673ab7',
+    blue: '#2196f3',
+    teal: '#009688',
+    green: '#4caf50',
+    lime: '#cddc39',
+    brown: '#795548',
 };
 
 const Block = styled(Paper)(({ theme }) => ({
-	backgroundColor:
-		theme.palette.mode === "dark"
-			? "rgba(27, 33, 40, 0.8)"
-			: "rgba(255, 255, 255, 0.8)",
-	...theme.typography.body2,
-	padding: theme.spacing(1),
-	textAlign: "left",
-	color: theme.palette.text.secondary,
+    backgroundColor:
+        theme.palette.mode === 'dark'
+            ? 'rgba(27, 33, 40, 0.8)'
+            : 'rgba(255, 255, 255, 0.8)',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
 }));
 
 /* welche daten gehören zu einem Member? */
@@ -55,167 +55,175 @@ const Block = styled(Paper)(({ theme }) => ({
 /* variable um dort alle gruppendaten einzulagern */
 
 export function ListGroupMember() {
-	return (
-		<>
-			<Block>
-				<h2>Aktuelle Spieler</h2>
+    return (
+        <>
+            <Block>
+                <h2>Aktuelle Spieler</h2>
+                <TableContainer component={Paper}>
+                    <Table
+                        sx={{
+                            minWidth: 250,
+                            backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                        }}
+                        aria-label="Spielerliste"
+                    >
+                        <TableHead></TableHead>
+                        <TableBody>
+                            {/* hier durch die verschiedenen Einträge loopen, eine Row definieren und dann wiederholen lassen */}
+							group.map()
+                            <TableRow>
+                                <TableCell align="left">
+                                    <Avatar
+                                        sx={{ bgcolor: avatarcolors.green }}
+                                    >
+                                        N
+                                    </Avatar>
+                                </TableCell>
+                                <TableCell align="left">Norbert</TableCell>
+                                <TableCell align="right">
+                                    <IconButton
+                                        color="error"
+                                        aria-label="Eintrag entfernen"
+                                        component="label"
+                                        onClick={() => {
+                                            alert('gelöscht');
+                                        }}
+                                    >
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
 
-				<TableContainer component={Paper}>
-					<Table
-						sx={{
-							minWidth: 250,
-							backgroundColor: "rgba(255, 255, 255, 0.5)",
-						}}
-						aria-label="Spielerliste"
-					>
-						<TableHead></TableHead>
-						<TableBody>
-							{/* hier durch die verschiedenen Einträge loopen, eine Row definieren und dann wiederholen lassen */}
-							<TableRow>
-								<TableCell align="left">
-									<Avatar
-										sx={{ bgcolor: avatarcolors.green }}
-									>
-										N
-									</Avatar>
-								</TableCell>
-								<TableCell align="left">Norbert</TableCell>
-								<TableCell align="left">
-									<IconButton
-										color="error"
-										aria-label="Eintrag entfernen"
-										component="label"
-										onClick={() => {
-											alert("gelöscht");
-										}}
-									>
-										<DeleteIcon />
-									</IconButton>
-								</TableCell>
-							</TableRow>
-
-							{/* testweise */}
-							<TableRow>
-								<TableCell align="left">
-									<Avatar
-										sx={{ bgcolor: avatarcolors.purple }}
-									>
-										R
-									</Avatar>
-								</TableCell>
-								<TableCell align="left">Roland</TableCell>
-								<TableCell align="left">
-									<IconButton
-										color="error"
-										aria-label="Eintrag entfernen"
-										component="label"
-										onClick={() => {
-											alert("gelöscht");
-										}}
-									>
-										<DeleteIcon />
-									</IconButton>
-								</TableCell>
-							</TableRow>
-						</TableBody>
-					</Table>
-				</TableContainer>
-			</Block>
-		</>
-	);
+                            {/* testweise */}
+                            <TableRow>
+                                <TableCell align="left">
+                                    <Avatar
+                                        sx={{ bgcolor: avatarcolors.purple }}
+                                    >
+                                        R
+                                    </Avatar>
+                                </TableCell>
+                                <TableCell align="left">Roland</TableCell>
+                                <TableCell align="right">
+                                    <IconButton
+                                        color="error"
+                                        aria-label="Eintrag entfernen"
+                                        component="label"
+                                        onClick={() => {
+                                            alert('gelöscht');
+                                        }}
+                                    >
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Block>
+        </>
+    );
 }
 
 export function AddGroupMember() {
-	// useState für farbe picken
-	const [color, setColor] = React.useState("");
+    // useState erstellt eine Liste von Type Player die Anfangs leer ist
+    // group ist eine Liste von Objekten des Type Plyer
+    const [group, setGroup] = useState<Player[]>([]);
 
-	const handleSetColor = (event: SelectChangeEvent) => {
-		setColor(event.target.value as string);
-	};
+    // useState for playerValues Object
+    const [selectedColor, setSelectedColor] = React.useState(avatarcolors.blue);
+    const [playername, setPlayername] = React.useState('');
 
-	// useState erstellt eine Liste von Type Player die Anfangs leer ist
-	// group ist eine Liste von Objekten des Type Plyer
-	const [group, setGroup] = useState<Player[]>([]);
+    const addPlayerToGroup = () => {
+        const newPlayer: Player = {
+            name: playername,
+            shortname:
+                playername.split(
+                    ' ',
+                )[0][0] /* + if(playername.includes(" ")) { playername.split(' ')[1][0] } */,
+            color: selectedColor,
+        };
+        console.log('name: ' + newPlayer.name);
+        console.log('sname: ' + newPlayer.shortname);
+        console.log('color: ' + newPlayer.color);
 
-	return (
-		<>
-			<Block sx={{}}>
-				<h2>Spieler hinzuf&uuml;gen</h2>
-				<FormControl>
-					{/* <InputLabel id="name-label">Name</InputLabel> */}
-					<TextField
-						required
-						id="outlined-required"
-						label="Spielername"
-						sx={{ marginBottom: 1 }}
-					/>
+        setGroup([...group, newPlayer]);
+        // possible "BUG": Wenn zB ein dritter Spieler hinzugefügt wird, zeigt die Ausgabe erstmal nur zwei an. Beim vierten, werden dann drei angezeigt usw
+        console.log(group);
+    };
 
-					{/* <InputLabel id="color-label">Color</InputLabel> */}
-
-					{/* <InputLabel id="demo-simple-select-label">Color</InputLabel> */}
-					<Select
-						/* labelId="demo-simple-select-label" */
-						id="demo-simple-select"
-						value={color}
-						label="Color"
-						onChange={handleSetColor}
-					>
-						<MenuItem
-							sx={{ bgcolor: avatarcolors.pink }}
-							value={avatarcolors.pink}
-						>
-							pink
-						</MenuItem>
-						<MenuItem
-							sx={{ bgcolor: avatarcolors.purple }}
-							value={avatarcolors.purple}
-						>
-							purple
-						</MenuItem>
-						<MenuItem
-							sx={{ bgcolor: avatarcolors.blue }}
-							value={avatarcolors.blue}
-						>
-							blue
-						</MenuItem>
-						<MenuItem
-							sx={{ bgcolor: avatarcolors.teal }}
-							value={avatarcolors.teal}
-						>
-							teal
-						</MenuItem>
-						<MenuItem
-							sx={{ bgcolor: avatarcolors.green }}
-							value={avatarcolors.green}
-						>
-							green
-						</MenuItem>
-						<MenuItem
-							sx={{ bgcolor: avatarcolors.lime }}
-							value={avatarcolors.lime}
-						>
-							lime
-						</MenuItem>
-						<MenuItem
-							sx={{ bgcolor: avatarcolors.brown }}
-							value={avatarcolors.brown}
-						>
-							brown
-						</MenuItem>
-					</Select>
-
-					<br></br>
-					<Button
-						variant="contained"
-						endIcon={<PersonAddIcon />}
-						onClick={() => {
-							alert("clicked");
-						}}
-					>
-						Add
-					</Button>
-				</FormControl>
-			</Block>
-		</>
-	);
+    return (
+        <>
+            <Block sx={{}}>
+                <h2>Spieler hinzuf&uuml;gen</h2>
+                <FormControl fullWidth>
+                    {/* <InputLabel id="name-label">Name</InputLabel> */}
+                    <TextField
+                        required
+                        label="Spielername"
+                        sx={{ marginBottom: 1 }}
+                        value={playername}
+                        onChange={(event) => setPlayername(event.target.value)}
+                    />
+                    <Select
+                        value={selectedColor}
+                        onChange={(event) =>
+                            setSelectedColor(event.target.value)
+                        }
+                    >
+                        <MenuItem
+                            sx={{ bgcolor: avatarcolors.pink }}
+                            value={avatarcolors.pink}
+                        >
+                            Pink
+                        </MenuItem>
+                        <MenuItem
+                            sx={{ bgcolor: avatarcolors.purple }}
+                            value={avatarcolors.purple}
+                        >
+                            Lila
+                        </MenuItem>
+                        <MenuItem
+                            sx={{ bgcolor: avatarcolors.blue }}
+                            value={avatarcolors.blue}
+                        >
+                            Blau
+                        </MenuItem>
+                        <MenuItem
+                            sx={{ bgcolor: avatarcolors.teal }}
+                            value={avatarcolors.teal}
+                        >
+                            Türkis
+                        </MenuItem>
+                        <MenuItem
+                            sx={{ bgcolor: avatarcolors.green }}
+                            value={avatarcolors.green}
+                        >
+                            Grün
+                        </MenuItem>
+                        <MenuItem
+                            sx={{ bgcolor: avatarcolors.lime }}
+                            value={avatarcolors.lime}
+                        >
+                            Limette
+                        </MenuItem>
+                        <MenuItem
+                            sx={{ bgcolor: avatarcolors.brown }}
+                            value={avatarcolors.brown}
+                        >
+                            Braun
+                        </MenuItem>
+                    </Select>
+                    <br></br>
+                    <Button
+                        variant="contained"
+                        endIcon={<PersonAddIcon />}
+                        onClick={addPlayerToGroup}
+                    >
+                        Add
+                    </Button>
+                </FormControl>
+            </Block>
+        </>
+    );
 }
