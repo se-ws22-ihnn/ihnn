34f89b394abd6905a24a3b3895eb351d780d1965
Jannik Hoefener <82703032+JannikHoefener@users.noreply.github.com>
2022-12-09 15:07:46 +0100

    einge gebene fragen tauchen jz bei frage zeigen auf

--- src/Components/ShowQuestion.tsx
+++ src/Components/ShowQuestion.tsx
@@ -5,22 +5,35 @@ import { useState } from 'react';
 import { GroupContext } from '../Context/GroupContext';
 
 export default function ShowQuestion() {
-    const { questionList, setQuestionList } = React.useContext(QuestionListContext);
+    const { questionList, setQuestionList } =
+        React.useContext(QuestionListContext);
     const { roundCounter, setRoundCounter } = React.useContext(GroupContext);
 
     const handleNewRound = () => {
+        // votings der runde muss in jedem spieler berücksichtig und übergeben werden werde (i did oder i didnt +1)
 
-        setRoundCounter(roundCounter + 1)
+        // zurücksetzten der voting slider
 
-    }
-    
+        // neu Frage anzeigen
+        // ===> Liste könnte jedes mal random befüllt werden und roundcounter wäre dann gleichzeitig der index mit dem man eine neue frage abruft
+        // ===> wenn ende der liste erreich, dann meldung und einfach wieder von vorne? oder spiel beenden?
+
+        // Roundcounter erhöhen
+        setRoundCounter(roundCounter + 1);
+    };
+
     return (
         <div>
-			<h2>Ich habe noch nie ...</h2>
-                <Typography>
-                    frage folgt hier
-                </Typography>
-				<Button variant='contained' onClick={handleNewRound}>Nächste Frage</Button>
+            <h2>Ich habe noch nie ...</h2>
+
+            {/* aktuell wird jede frage in der liste angezeigt, da noch nicht auf einen index gezeigt wird */}
+            {questionList.map((currentQuestion, roundCounter) => (
+                <Typography>{currentQuestion.questionText}</Typography>
+            ))}
+
+            <Button variant="contained" onClick={handleNewRound}>
+                Nächste Frage
+            </Button>
         </div>
     );
 }
