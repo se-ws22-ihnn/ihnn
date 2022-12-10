8092a2a51f82a5fc6b952a3f4b5c8a488473e8ea
Jannik Hoefener <82703032+JannikHoefener@users.noreply.github.com>
2022-12-10 20:48:49 +0100

    es lässt sich jz eine neue frage aus der liste wechseln
    #30 alles wurde in github im issue beschrieben

--- src/Components/ShowQuestion.tsx
+++ src/Components/ShowQuestion.tsx
@@ -1,39 +1,43 @@
 import * as React from 'react';
 import { Button, Typography } from '@mui/material';
 import { QuestionListContext } from '../Context/QuestionsListContext';
-import { useState } from 'react';
 import { GroupContext } from '../Context/GroupContext';
 
+
+// diese Komponente muss mit Gorupvoting zusammengelegt werden, damit handleNewRound alle Werte erhält
+
 export default function ShowQuestion() {
-    const { questionList, setQuestionList } =
-        React.useContext(QuestionListContext);
+    const { questionList } = React.useContext(QuestionListContext);
     const { roundCounter, setRoundCounter } = React.useContext(GroupContext);
 
     const handleNewRound = () => {
-        // votings der runde muss in jedem spieler berücksichtig und übergeben werden werde (i did oder i didnt +1)
-
-        // zurücksetzten der voting slider
-
-        // neu Frage anzeigen
+        // 1. wenn gruppe angegeben hat wie viele runden gespielt werden sollen muss das hier per if berücksichtigt werden
+        // 2. votings der runde muss in jedem spieler berücksichtig und übergeben werden werde (i did oder i didnt +1)
+        // 3.zurücksetzten der voting slider
+        // 4. neu Frage anzeigen oder direkt im return() ????
+                // BUG: wenn liste leer bzw durch, wird nichts mehr gerendert
         // ===> Liste könnte jedes mal random befüllt werden und roundcounter wäre dann gleichzeitig der index mit dem man eine neue frage abruft
         // ===> wenn ende der liste erreich, dann meldung und einfach wieder von vorne? oder spiel beenden?
 
-        // Roundcounter erhöhen
+        // 5. Roundcounter erhöhen
         setRoundCounter(roundCounter + 1);
     };
 
     return (
-        <div>
+        <>
             <h2>Ich habe noch nie ...</h2>
+            <Typography>
+                {questionList[roundCounter - 1].questionText}
+            </Typography>
 
             {/* aktuell wird jede frage in der liste angezeigt, da noch nicht auf einen index gezeigt wird */}
-            {questionList.map((currentQuestion, roundCounter) => (
+            {/* {questionList.map((currentQuestion, roundCounter) => (
                 <Typography>{currentQuestion.questionText}</Typography>
-            ))}
+            ))} */}
 
             <Button variant="contained" onClick={handleNewRound}>
                 Nächste Frage
             </Button>
-        </div>
+        </>
     );
 }
--- src/Context/QuestionsListContext.tsx
+++ src/Context/QuestionsListContext.tsx
@@ -5,27 +5,45 @@ import { Question } from '../types/questionType';
 type ContextProps = {
     questionList: Question[];
     setQuestionList: (value: any) => void;
-  }
+};
 //context variable with initial values
 const initValues: ContextProps = {
     questionList: [],
     setQuestionList: function (value: string): void {
         throw new Error('Function not implemented.');
-    }
-}
+    },
+};
 export const QuestionListContext = createContext<ContextProps>(initValues);
 
 // ===> Provider of the Context
 type ProviderProps = {
     children?: React.ReactNode;
-  };
+};
 export const QuestionListContextProvider = (props: ProviderProps) => {
-    // useState erstellt eine Liste von Type Player die Anfangs leer ist
-    // group ist eine Liste von Objekten des Type Player
-    const [questionList, setQuestionList] = useState<Question[]>([]);
 
-    /* Liste mit beispiel fragen befüllen */
-    /* setQuestionList([]) */
+    // Liste soll bereits test bzw default fragen als inital wert bekommmen!
+    const initalQuestionList = [
+        {
+            id: 1,
+            questionText: 'Das ist Testfrage 1',
+            kategorie: 'initalValue',
+        },
+        {
+            id: 2,
+            questionText: 'Das ist Testfrage 2',
+            kategorie: 'initalValue',
+        },
+        {
+            id: 3,
+            questionText: 'Das ist Testfrage 3',
+            kategorie: 'initalValue',
+        },
+    ];
+
+    // erstellung der Frageliste und zuweisung der default fragen per useState
+    const [questionList, setQuestionList] = useState<Question[]>(initalQuestionList);
+
+
 
     return (
         <QuestionListContext.Provider
