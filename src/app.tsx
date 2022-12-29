0b627ca25d2a68359ed32a93e3b7f707dfecae94
Nico B <nico.bielat@haw-hamburg.de>
2022-12-29 19:18:08 +0100

    Am Styling gearbeitet #45

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider, Typography } from '@mui/material';
+import { Box, ThemeProvider, Typography } from '@mui/material';
 import './index.css';
 import ButtonAppBar from './Components/Header';
 import { GroupContextProvider } from './Context/GroupContext';
@@ -9,15 +9,21 @@ import GameStateMachine from './States/GameStateMachine';
 /* adding a global visible background image to the page */
 /* load it with  <div style={background.image}> */
 
-let w = window.innerWidth;
-let h = window.innerHeight;
-const background = {
-    image: {
-        minHeight: '100vh',
-        backgroundColor: '#BFB59E',
-        backgroundSize: w + 'px ' + h + 'px',
-    },
-};
+// let w = window.innerWidth;
+// let h = window.innerHeight;
+// const background = {
+//     image: {
+//         minHeight: '100vh',
+//         backgroundColor: '#BFB59E',
+//         backgroundSize: w + 'px ' + h + 'px',
+//     },
+//     image2: {
+//         backgroundImage: "url(images/logo512.png)",
+//         backgroundRepeat: 'no-repeat',
+//     },
+// };
+
+
 
 
 export default function App() {
@@ -26,11 +32,34 @@ export default function App() {
             {/* Hier unsere Components */}
             <QuestionListContextProvider>
                 <GroupContextProvider>
+                
                     <div 
-                        style={background.image} >
+                        style={{ backgroundImage: "url(images/logo512.png), url(images/light-noise-background.png)",
+                        
+                        minHeight: '100vh', 
+                        backgroundRepeat: 'no-repeat, repeat',
+                        backgroundPosition: 'center 100px',
+                        backgroundSize: '600px, 50px',
+                        }}>
+
+
+                        {/* style={background.image}  */}
+                        
                             
                         <ButtonAppBar />
                         <GameStateMachine />
+                        {/* <Box
+                                    component="img"
+                                    alt="IHNN Logo"
+                                    src="images/logo512.png"
+                                    minHeight= '10vh'
+                                    maxHeight= '100vh'
+                                    zIndex="-1"
+                                    />
+                                    
+                        <Box/>  */}
+
+                
                     </div>
                     
                 </GroupContextProvider>
