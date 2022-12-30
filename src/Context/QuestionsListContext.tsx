53ccbc7767365f8cdcfa84c5d1400331cd06b34f
Soeren Helms <soeren.helms1@gmail.com>
2022-12-30 15:31:02 +0100

    #44 erste daten in die Testversion eingebaut
    da die DB Anbindung noch nicht klappt...

--- src/Context/QuestionsListContext.tsx
+++ src/Context/QuestionsListContext.tsx
@@ -25,19 +25,85 @@ export const QuestionListContextProvider = ({children}: ProviderProps) => {
     const initalQuestionList = [
         {
             id: 1,
-            questionText: 'Das ist Testfrage 1',
-            kategorie: 'initalValue',
-        },
-        {
+            questionText: "Ich war noch nie Fallschirm springen",
+            kategorie: "adventure"
+          },{
             id: 2,
-            questionText: 'Das ist Testfrage 2',
-            kategorie: 'initalValue',
-        },
-        {
+            questionText: "Ich habe noch nie ohne Zelt im Wald übernachtet",
+            kategorie: "adventure"
+          },{
             id: 3,
-            questionText: 'Das ist Testfrage 3',
-            kategorie: 'initalValue',
-        },
+            questionText: "Ich habe noch nie einen Lost Place besucht",
+            kategorie: "adventure"
+          },{
+            id: 4,
+            questionText: "Ich bin noch nie von einer Klippe gesprungen",
+            kategorie: "adventure"
+          },{
+            id: 5,
+            questionText: "Ich bin noch nie Jetski gefahren",
+            kategorie: "adventure"
+          },{
+            id: 6,
+            questionText: "Ich habe noch nie in einem Iglu übernachtet",
+            kategorie: "adventure"
+          },{
+            id: 7,
+            questionText: "Ich habe noch nie in einem Wasserfall geduscht",
+            kategorie: "adventure"
+          },{
+            id: 8,
+            questionText: "Ich habe noch nie den Eiffelturm besucht",
+            kategorie: "travel"
+          },{
+            id: 9,
+            questionText: "Ich habe noch nie alle Kontinente bereist",
+            kategorie: "travel"
+          },{
+            id: 10,
+            questionText: "Ich habe noch nie eine Safari in Afrika gemacht",
+            kategorie: "travel"
+          },{
+            id: 11,
+            questionText: "Ich habe noch nie das Empire State Building besucht",
+            kategorie: "travel"
+          },{
+            id: 12,
+            questionText: "Ich habe noch nie die chinesische Mauer besucht",
+            kategorie: "travel"
+          },{
+            id: 13,
+            questionText: "Ich habe noch nie die ägyptischen Pyramiden besucht",
+            kategorie: "travel"
+          },{
+            id: 14,
+            questionText: "Ich habe noch nie die Casinos von Las Vegas besucht",
+            kategorie: "travel"
+          },{
+            id: 15,
+            questionText: "Ich habe noch nie einen Tauchschein gemacht",
+            kategorie: "sport"
+          },{
+            id: 16,
+            questionText: "Ich habe noch nie 50 Liegestütze am Stück geschafft",
+            kategorie: "sport"
+          },{
+            id: 17,
+            questionText: "Ich habe noch nie 100 km gehen in 24 Stunden geschafft",
+            kategorie: "sport"
+          },{
+            id: 18,
+            questionText: "Ich habe noch nie einen Baum gepflanzt",
+            kategorie: "life"
+          },{
+            id: 19,
+            questionText: "Ich habe noch nie Blut gespendet",
+            kategorie: "life"
+          },{
+            id: 20,
+            questionText: "Ich habe noch nie ein Tier aus einem Tierheim aufgenommen",
+            kategorie: "life"
+          }
     ];
 
     // erstellung der Frageliste und zuweisung der default fragen per useState
